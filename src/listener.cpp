1591457697d1ed70d180103cd58c23170fbbce90
kevinchen <kevin.chen@innopeaktech.com>
2018-12-20 10:01:06 -0800

    First init

---
+++ src/listener.cpp
@@ -0,0 +1,18 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+
+void chatterCallBack(const std_msgs::String::ConstPtr& msg)
+{
+    ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "listener");
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallBack);
+    ros::spin();
+
+    return 0;
+}
